69f0172d20bac95af67d69abaa870249858be381
Nomfundo <mpirijo@outlook.com>
2025-01-14 14:06:50 +0200

    Add files via upload

---
+++ COMproperties.dart
@@ -0,0 +1,49 @@
+---Kidney Processes and Use---
+   |----------------------|
+   
+Ⅰ Uses --
+
+⩺Filter the blood
+⩺ Remove waste we dont want called Urea 
+made in the liver:'Deamination'
+⩺Its main job is to store fats and carbohydrates
+⩺Regulate the levels of useful things:
+⅟First things they regulate Water & Ions as 
+ↇSodium 
+ↇPotassium 
+i.e if they to high
+Which result from diet and other gi..
+
+Ⅱ Water Uses
+
+⩺Water cleans blood in the kidney with a process and water is absorbed to the cell or lost, that process is called Osmosis.
+⩺
+
+Ⅲ Kidney Connection
+
+⩺Kidneys are connected by tubes called Nephrons & Tubule
+¾Tubule
+₥.[ↈ"Water";
+      ↈ"Glucose";
+         ↈ"Amino Acids";
+            ↈ"Urea"৻
+
+
+],
+   Ren True;
+
+৲Filtration৻₮
+
+Reabsorbs water ellement  // .xml
+
+Ⅳ How Water "_connects_or_regulations"
+
+⩺$.Hypothalamus
+
+Ξ Report water level are too low
+Ξ Send and Receive ‰‱POLAR"Signal to the Pituitary Gland[Follow Link]";
+⋙∭⋓ Release hormone : 'Anti--Diuretic Hormone' ADH 
+-If water level is to high adh will stop which means connection lost.
+-Negetive feedback loop will keep our blood and water concentration equilibrium ↆ
+≛
+
